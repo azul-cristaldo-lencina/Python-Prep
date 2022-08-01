2b0d280d65f86c56293b530f032d07268c4bebfc
Chuny10 <you@example.com>
2022-08-01 16:09:03 -0300

    homework done

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -1,12 +1,79 @@
-mi_variable = 12
-print(mi_variable)
+# num = 10
+# print(num)
+# print(type(num))
 
-mi_variable2 = 'dario'
-print(mi_variable2)
+# dato = type(8.5)
+# print(dato)
 
-mi_complejo = 5 + 7j
-print(mi_complejo)
+# mi_nombre = 'Azul'
 
+# num_complejo = 7 + 8j
+# print(type(num_complejo))
 
-mi_numero = input('Ingrese un valor')
-print('el valor ingresado fue', mi_numero)
+# n_pi = 3.14
+# print(n_pi)
+# print(int(n_pi))
+
+# hace_frio = True
+# es_invierno = True
+# print(type(hace_frio))
+
+# suma = 10 + 4.5
+# print(suma)
+
+# suma_comp = 5j + 8j
+# print(suma_comp)
+
+# suma_1 = 4 + 1j
+# # print(suma_1)
+
+# mult = 5 * 5
+# print(mult)
+
+# pote = 2 ** 8
+# print(pote)
+
+# cociente = 27 / 4
+# print(cociente)
+# print(int(cociente))
+
+# operando = 4 * 6 + 3
+# operando_1 = 4 * 6.75
+# print(operando)
+# print(operando_1)
+
+# a = 'Hoy es invierno '
+# b = 'hace mucho frío'
+# print(a + b)
+
+# evaluar = "2"
+# evaluar = int(evaluar)
+# print(type(evaluar))
+# eval = 2
+# print(eval == evaluar)
+
+#22) ¿Por qué arroja error el siguiente cambio de tipo de datos? a = float('3,8')
+
+#a = float('3,8') #no se puede convertir a float un sting. Sirve solo si es un float o entero
+#print(a)
+
+# var = 3
+# var -= 3
+# print(var)
+
+# op = 1 << 2
+# print(op)
+
+#25) Realizar la operación 2 + '2' ¿Por qué no está permitido? ¿Si los dos operandos serían del mismo tipo, siempre arrojaría el mismo resultado?
+
+# a = 2
+# b = '2'
+# print( a + b) ##no se puede sumar ni concatenar distintos valores. si fueron 2+2 daría 4, pero si fuera '2' + '2' =22, daria 22 pero en string poque estariamos concatenando strings.
+
+c = 2
+d = 2
+print(c + d)
+
+e = '2'
+f = '2'
+print( e + f)
